03f334f0175742a5ddd3e3d1f507ffd77f0bd22f
Sean McGillis <slcidevice@yahoo.com>
2017-03-27 21:18:32 -0600

    added ReactiveFormsModule import to appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
 
@@ -29,6 +29,7 @@ export const firebaseConfig = {
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     NotesModule,
     AngularFireModule.initializeApp(firebaseConfig),
